41a50db5f96cb0d04e84f794e2a995e2babab027
Akash667 <akashrawatcats@gmail.com>
2020-05-05 14:13:38 +0530

    Cards added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import FlashcardList from './FlashcardList';
+import './app.css';
+
 
 function App() {
+
+    const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <h1>
+     <FlashcardList  flashcards={flashcards}/>
+   </h1>
   );
 }
+
+
+const SAMPLE_FLASHCARDS = [
+  {
+      id:1,
+      question:'What is 2+2?',
+      answer :'4',
+      options:[
+          '2','3','4','5'
+      ]   
+  },
+  {
+      id:1,
+      question:'Question 2?',
+      answer :'Meme',
+      options:[
+          'Answer','Answer','Answer','Answer'
+      ]   
+  }
+ 
+]
+
+
 
 export default App;
